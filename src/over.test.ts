0cd00e1f17a0612f74b67f0080f2ded530dd3120
ndthanhdev <ndthanhdev@outlook.com>
2022-03-18 16:13:28 +0700

    update test

--- src/over.test.ts
+++ src/over.test.ts
@@ -1,8 +1,45 @@
 import { over } from "./over";
 
-it("ok sync", () => {});
-it("ok async", async () => {});
-it("error sync", () => {});
-it("error async", async () => {});
+function okSync(name: string) {
+	return name;
+}
 
-it("should be true", () => expect(true).toBe(true));
+function errorSync(name: string) {
+	throw new Error(name);
+}
+
+function okAsync(name: string) {
+	return Promise.resolve(name);
+}
+
+function errorAsync(name: string) {
+	return Promise.reject(new Error(name));
+}
+
+it("sync ok", () => {
+	expect(over(okSync, "value")).toEqual({
+		ok: true,
+		value: "value",
+	});
+});
+
+it("sync error", () => {
+	expect(over(errorSync, "reason")).toEqual({
+		ok: false,
+		reason: new Error("reason"),
+	});
+});
+
+it("async ok", async () => {
+	await expect(over(okAsync, "value")).resolves.toEqual({
+		ok: true,
+		value: "value",
+	});
+});
+
+it("async error", async () => {
+	await expect(over(errorAsync, "reason")).resolves.toEqual({
+		ok: false,
+		reason: new Error("reason"),
+	});
+});
--- src/over.ts
+++ src/over.ts
@@ -1,12 +1,10 @@
 export type OKValue<V> = {
-  ok: true;
-  error: false;
-  value: V;
+	ok: true;
+	value: V;
 };
 export type ErrorReason<R> = {
-  ok: false;
-  error: true;
-  reason: R;
+	ok: false;
+	reason: R;
 };
 
 export type OVER<V, E> = OKValue<V> | ErrorReason<E>;
@@ -15,55 +13,49 @@ export type AsyncOVER<V, E> = Promise<OVER<V, E>>;
 type Fn<V = any> = (...args: any[]) => V;
 
 function over<TValue = any, TReason = unknown>(
-  fn: Fn<TValue>,
-  ...params: Parameters<typeof fn>
+	fn: Fn<TValue>,
+	...params: Parameters<typeof fn>
 ): TValue extends Promise<infer TPValue>
-  ? AsyncOVER<TPValue, TReason>
-  : OVER<TValue, TReason>;
+	? AsyncOVER<TPValue, TReason>
+	: OVER<TValue, TReason>;
 function over<TFn extends Fn = Fn, TReason = unknown>(
-  fn: TFn,
-  ...params: Parameters<TFn>
+	fn: TFn,
+	...params: Parameters<TFn>
 ): ReturnType<TFn> extends Promise<infer TPValue>
-  ? AsyncOVER<TPValue, TReason>
-  : OVER<ReturnType<TFn>, TReason>;
+	? AsyncOVER<TPValue, TReason>
+	: OVER<ReturnType<TFn>, TReason>;
 function over(fn: Fn<any>, ...params: Parameters<typeof fn>): any {
-  try {
-    let temp = fn(...params);
+	try {
+		let temp = fn(...params);
 
-    if (temp instanceof Promise) {
-      // @ts-ignore
-      return temp
-        .then((result) => {
-          return {
-            ok: true,
-            error: false,
-            result: result,
-          };
-        })
-        .catch((reason) => {
-          return {
-            ok: false,
-            error: true,
-            reason,
-          };
-        });
-    }
+		if (temp instanceof Promise) {
+			// @ts-ignore
+			return temp
+				.then((result) => {
+					return {
+						ok: true,
+						value: result,
+					};
+				})
+				.catch((reason) => {
+					return {
+						ok: false,
+						reason,
+					};
+				});
+		}
 
-    // @ts-ignore
-    return {
-      ok: true,
-      error: false,
-      value: temp,
-    };
-  } catch (reason) {
-    // @ts-ignore
-    return {
-      ok: false,
-      error: true,
-      // @ts-ignore
-      reason,
-    };
-  }
+		// @ts-ignore
+		return {
+			ok: true,
+			value: temp,
+		};
+	} catch (reason) {
+		return {
+			ok: false,
+			reason,
+		};
+	}
 }
 
 export { over };
