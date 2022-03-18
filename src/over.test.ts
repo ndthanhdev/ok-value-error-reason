7dc8e20e74bc04b30810ab45b52af211346872b9
ndthanhdev <ndthanhdev@outlook.com>
2022-03-18 15:15:46 +0700

    add build script

--- src/over.test.ts
+++ src/over.test.ts
@@ -1,1 +1,8 @@
+import { over } from "./over";
+
+it("ok sync", () => {});
+it("ok async", async () => {});
+it("error sync", () => {});
+it("error async", async () => {});
+
 it("should be true", () => expect(true).toBe(true));
